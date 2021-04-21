6e87435da27c54b7791e97e2447cbd01b24b2eb7
hktrollgames <hktrollgames@gmail.com>
2021-04-22 00:39:30 +0900

    계속하시겠습니까? 질문 샘플 프로젝트 추가

---
+++ ExitQuestion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExitQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 키보드를 눌러주세요
+            // 누르신 키보드는 ()입니다
+            // 계속 하시겠습니까 ? (y / n)
+
+            bool isContinue;
+            do
+            {
+                Print("키보드를 눌러주세요");
+                string inputKey = Console.ReadLine();
+                Print($"누르신 키는 {inputKey}입니다.");
+                Print("계속 하시겠습니까?(y/n)");
+
+                inputKey = Console.ReadLine();
+                isContinue = inputKey == "y" || inputKey == "Y";
+            } while (isContinue);        
+        }
+
+        private static void Print(string log)
+        {
+            Console.WriteLine(log);
+        }
+    }
+}
