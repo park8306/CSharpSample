435c65c16d0d03add5bbae16444274eee6e4ba2f
hktrollgames <hktrollgames@gmail.com>
2021-04-21 19:27:48 +0900

    초기 커밋.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("출력할 구구단을 입력하세요");
+
+            string readString = Console.ReadLine();
+
+            int number = int.Parse(readString);
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"{number} X {i + 1} = {number * (i + 1)}");
+            }
+        }
+    }
+}
+
