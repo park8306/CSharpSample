24cd223653c4e342d59797fc2ecd7b50740ebf20
DESKTOP-PF6LLID\오후F <hktrollgames@gmail.com>
2021-04-23 16:13:37 +0900

    list1 초기화 방법

---
+++ ListSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+
+            // list1 초기화 방법
+            List<string> list1 = new List<string>();
+            list1.Add("1");
+            list1.Add("aa");
+            list1.Add("22");
+
+
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
